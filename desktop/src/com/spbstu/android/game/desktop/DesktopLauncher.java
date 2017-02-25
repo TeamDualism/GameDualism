3f3b247f75401f16b2ff6941be7cc5fe4f923066
enotka <shabalina.a.vl@gmail.com>
2017-02-25 15:10:30 +0300

    Add empty generated libgdx project

---
+++ desktop/src/com/spbstu/android/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spbstu.android.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spbstu.android.game.GameDualism;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameDualism(), config);
+	}
+}
