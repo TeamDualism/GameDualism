c39c78e703b5e8c834bb59e35bba66d35654d897
LesikDee <lesikdjem@yandex.ru>
2017-03-06 04:22:32 +0300

    Add files via upload

--- desktop/src/com/spbstu/android/game/desktop/DesktopLauncher.java
+++ desktop/src/com/spbstu/android/game/desktop/DesktopLauncher.java
@@ -1,11 +1,12 @@
 package com.spbstu.android.game.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.spbstu.android.game.GameDualism;
-
+import java.io.*;
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] arg)   {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = GameDualism.WIDTH;
 		config.height = GameDualism.HEIGHT;
