73e767d59bc7fd8dc64384c42f0382ca1af79cd0
Pavel Zatylkin <paulztk@gmail.com>
2017-03-19 18:00:52 +0300

    [misc] Add pixel to meter coefficient

--- core/src/com/spbstu/android/game/Screens/Level1Screen.java
+++ core/src/com/spbstu/android/game/Screens/Level1Screen.java
@@ -24,6 +24,8 @@ import com.spbstu.android.game.GameDualism;
 import com.spbstu.android.game.MapParser;
 import com.spbstu.android.game.Player;
 
+import static com.spbstu.android.game.MapParser.PPM;
+
 public class Level1Screen extends ScreenAdapter {
 
     private TiledMap map;
@@ -58,7 +60,9 @@ public class Level1Screen extends ScreenAdapter {
         game.assetManager.finishLoading();
         batch = new SpriteBatch();
         world = new World(new Vector2(0, -20f), false);
-        player = new Player(0.8f, 0.8f + 1.6f * 3, 1.5f, world, game.assetManager);
+        player = new Player(16f / (2 * PPM),
+                16f / (2 * PPM) + 16 / PPM * 3,
+                (16 / PPM - 0.1f) / 2, world, game.assetManager);
         MapParser.parseMapObjects(map.getLayers().get("Line").getObjects(), world);
         actionButtons();
     }
@@ -189,10 +193,10 @@ public class Level1Screen extends ScreenAdapter {
             stage.draw();
             batch.begin();
             batch.draw(player.texture,
-                    player.body.getPosition().x * 10 - player.texture.getWidth() / 2,
-                    player.body.getPosition().y * 10 - player.texture.getHeight() / 2);
+                    player.body.getPosition().x * PPM - player.texture.getWidth() / 2,
+                    player.body.getPosition().y * PPM - player.texture.getHeight() / 2);
             batch.end();
-            //box2DDebugRenderer.render(world, camera.combined.scl(10));//надо только в дебаге
+            //box2DDebugRenderer.render(world, camera.combined.scl(PPM));//надо только в дебаге
 
         }
         else {
@@ -238,18 +242,18 @@ public class Level1Screen extends ScreenAdapter {
     }
 
     private void moveCamera() {
-        camera.position.set(player.body.getPosition().x * 10f, player.body.getPosition().y * 10f, camera.position.z);
+        camera.position.set(player.body.getPosition().x * PPM, player.body.getPosition().y * PPM, camera.position.z);
 
-        if (player.body.getPosition().x - Gdx.graphics.getWidth() / (90f) < 0)
+        if (player.body.getPosition().x - Gdx.graphics.getWidth() / (9f * PPM) < 0)
             camera.position.set(Gdx.graphics.getWidth() / 9f, camera.position.y, camera.position.z);
 
-        if (player.body.getPosition().x + Gdx.graphics.getWidth() / (90f) > map.getProperties().get("width", Integer.class) * 1.6f )
+        if (player.body.getPosition().x + Gdx.graphics.getWidth() / (9f * PPM) > map.getProperties().get("width", Integer.class) * 16 / PPM )
             camera.position.set(map.getProperties().get("width", Integer.class) * 16f - Gdx.graphics.getWidth() / 9f, camera.position.y, camera.position.z);
 
-        if (player.body.getPosition().y - Gdx.graphics.getHeight() / (90f) < 0)
+        if (player.body.getPosition().y - Gdx.graphics.getHeight() / (9f * PPM) < 0)
             camera.position.set(camera.position.x, Gdx.graphics.getHeight() / 9f, camera.position.z);
 
-        if (player.body.getPosition().y + Gdx.graphics.getHeight() / (90f) > map.getProperties().get("height", Integer.class) * 1.6f )
+        if (player.body.getPosition().y + Gdx.graphics.getHeight() / (9f * PPM) > map.getProperties().get("height", Integer.class) * 16f / PPM)
             camera.position.set(camera.position.x, map.getProperties().get("height", Integer.class) * 16f - Gdx.graphics.getHeight() / 9f, camera.position.z);
     }
 }
