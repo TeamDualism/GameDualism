3ba9a4fe4fa8378f2efd5bea279e93af2df49939
Pavel Zatylkin <paulztk@gmail.com>
2017-03-29 00:36:51 +0300

    [1-40] Add bonus

---
+++ core/src/com/spbstu/android/game/objects/Bonus.java
@@ -0,0 +1,32 @@
+package com.spbstu.android.game.objects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.spbstu.android.game.MapParser;
+
+public class Bonus extends Object{
+    public Bonus(float x, float y, Texture texture, World world) {
+        super(x, y, texture, world);
+    }
+
+    protected void createBody(float x, float y) {
+        BodyDef bodyDef = new BodyDef();
+
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set(x + this.getWidth() / (2 * MapParser.PPM), y + this.getHeight() / (2 * MapParser.PPM));
+        bodyDef.fixedRotation = true;
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(this.getHeight() / (2 * MapParser.PPM));
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.isSensor = true;
+
+        body.createFixture(fixtureDef);
+        shape.dispose();
+    }
+}
