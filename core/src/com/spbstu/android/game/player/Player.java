6e32fa98e22e109035c5f377b4551ea7643cd7c2
Pavel Zatylkin <paulztk@gmail.com>
2017-04-02 23:40:56 +0300

    [1-43] Add player animation

--- core/src/com/spbstu/android/game/player/Player.java
+++ core/src/com/spbstu/android/game/player/Player.java
@@ -1,8 +1,13 @@
 package com.spbstu.android.game.player;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -14,6 +19,9 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 
+import static com.spbstu.android.game.player.Player.Direction.LEFT;
+import static com.spbstu.android.game.player.Player.Direction.RIGHT;
+import static com.spbstu.android.game.player.Player.State.STANDING;
 import static com.spbstu.android.game.utils.Constants.IMPULSE;
 import static com.spbstu.android.game.utils.Constants.MAX_VELOCITY;
 import static com.spbstu.android.game.utils.Constants.PLAYER_BIT;
@@ -22,12 +30,22 @@ import static com.spbstu.android.game.utils.Constants.SENSOR_BIT;
 import static com.spbstu.android.game.utils.Constants.STOP;
 
 public class Player {
-    public Texture texture;
+    private TextureAtlas atlas;
     public Body body;
     public int jumpNumber;
     private int bonusCounter;
 
-    public Player(float x, float y, float radius, World world, AssetManager assetManager) {
+    float stateTime;
+    private Animation<TextureRegion> runningAnimation;
+    private Animation<TextureRegion> standingAnimation;
+    private Animation<TextureRegion> jumpingAnimation;
+
+    public enum State {STANDING, RUNNING, JUMPING};
+    public enum Direction {LEFT, RIGHT};
+    private State state;
+    private Direction direction;
+
+    public Player(float x, float y, float radius, World world) {
         BodyDef bodyDef = new BodyDef();
 
         bodyDef.type = BodyDef.BodyType.DynamicBody;
@@ -56,13 +74,22 @@ public class Player {
         body.createFixture(fixtureDef);
         shape1.dispose();
 
-        texture = assetManager.get("Textures/character.png", Texture.class);
+        atlas = new TextureAtlas(new FileHandle("Textures/player.pack"));
+        runningAnimation = new Animation<TextureRegion>(0.2f, atlas.findRegions("running"), Animation.PlayMode.LOOP);
+        standingAnimation = new Animation<TextureRegion>(0.2f, atlas.findRegions("standing"), Animation.PlayMode.LOOP);
+        jumpingAnimation = new Animation<TextureRegion>(0.2f, atlas.findRegions("jumping"), Animation.PlayMode.LOOP);
 
         jumpNumber = 1;
         bonusCounter = 0;
+
+        stateTime = 0f;
+        state = STANDING;
+        direction = RIGHT;
     }
 
     public void moveRight() {
+        direction = RIGHT;
+
         body.applyLinearImpulse(IMPULSE, 0, body.getPosition().x, body.getPosition().y, false);
 
         if (Math.abs(body.getLinearVelocity().x) > MAX_VELOCITY) {
@@ -71,6 +98,8 @@ public class Player {
     }
 
     public void moveLeft() {
+        direction = LEFT;
+
         body.applyLinearImpulse(-IMPULSE, 0, body.getPosition().x, body.getPosition().y, false);
 
         if (Math.abs(body.getLinearVelocity().x) > MAX_VELOCITY) {
@@ -88,17 +117,52 @@ public class Player {
     }
 
     public void stop() {
-        body.setLinearVelocity(body.getLinearVelocity().x * STOP, body.getLinearVelocity().y);
+        //body.setLinearVelocity(body.getLinearVelocity().x * STOP, body.getLinearVelocity().y);
+        body.setLinearVelocity(0f, body.getLinearVelocity().y);
     }
 
 
     public void render(SpriteBatch batch) {
+        stateTime += Gdx.graphics.getDeltaTime();
+        TextureRegion currentFrame;
+
+        switch (state) {
+            case RUNNING:
+                currentFrame = runningAnimation.getKeyFrame(stateTime, true);
+                break;
+            case STANDING:
+                currentFrame = standingAnimation.getKeyFrame(stateTime, true);
+                break;
+            case JUMPING:
+                currentFrame = jumpingAnimation.getKeyFrame(stateTime, true);
+                break;
+            default:
+                currentFrame = standingAnimation.getKeyFrame(stateTime, true);
+                break;
+        }
         batch.begin();
-        batch.draw(texture,
-                body.getPosition().x * PPM - texture.getWidth() / 2,
-                body.getPosition().y * PPM - texture.getHeight() / 2);
+
+        batch.draw(currentFrame,
+                body.getPosition().x * PPM - currentFrame.getRegionWidth() / 2 + (direction == LEFT ? currentFrame.getRegionWidth() : 0),
+                body.getPosition().y * PPM - currentFrame.getRegionHeight() / 2,
+                currentFrame.getRegionWidth() * (direction == LEFT ? -1 : 1),
+                currentFrame.getRegionHeight());
         batch.end();
     }
+
+    public boolean isGrounded(World world) {
+        Fixture sensorFixture = body.getFixtureList().get(1);
+
+        Array<Contact> contactList = world.getContactList();
+
+        for (Contact contact : contactList) {
+            if (contact.isTouching() && (contact.getFixtureA() == sensorFixture || contact.getFixtureB() == sensorFixture))
+                return true;
+        }
+
+        return false;
+    }
+
 
     public int getTileX() {
         return (int)Math.floor(body.getPosition().x);
@@ -114,5 +178,9 @@ public class Player {
 
     public int getBonusCounter() {
         return bonusCounter;
+    }
+
+    public void setState(State newState) {
+        state = newState;
     }
 }
