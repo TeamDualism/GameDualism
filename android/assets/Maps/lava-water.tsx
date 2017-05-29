96ed0247dae7e174f0761b270c9fca50ad9056f8
DimaDemyanov <dima.demyanov.1997@gmail.com>
2017-05-29 03:29:07 +0300

    [1-54] level added

---
+++ android/assets/Maps/lava-water.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lava-water" tilewidth="16" tileheight="16" tilecount="40" columns="8">
+ <image source="Tiles/lava-water.png" width="128" height="80"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="0" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="4" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="8" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="12" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="18" duration="200"/>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="16" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="21" duration="200"/>
+   <frame tileid="20" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="27">
+  <animation>
+   <frame tileid="26" duration="200"/>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="24" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="31">
+  <animation>
+   <frame tileid="30" duration="200"/>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="28" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="35">
+  <animation>
+   <frame tileid="34" duration="200"/>
+   <frame tileid="33" duration="200"/>
+   <frame tileid="32" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="39">
+  <animation>
+   <frame tileid="38" duration="200"/>
+   <frame tileid="37" duration="200"/>
+   <frame tileid="36" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
